fbd54e8e47fe88aadf9c34b502443f8985f84e77
Jessica Chan <jessica.j.chan@stonybrook.edu>
2021-01-21 15:46:56 -0500

    Roger Forgot Password

--- client/src/App.js
+++ client/src/App.js
@@ -1,80 +1,86 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-import axios from "axios";
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
+import axios from 'axios';
 // import { Provider } from "react-redux";
 
-
 import './App.css';
-import Welcome from "./components/layout/Welcome";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Dashboard from "./components/layout/Dashboard";
-import Host from "./components/game/Host";
-import Join from "./components/game/Join";
-import FoundBug from "./components/layout/FoundBug";
+import Welcome from './components/layout/Welcome';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/layout/Dashboard';
+import Host from './components/game/Host';
+import Join from './components/game/Join';
+import FoundBug from './components/layout/FoundBug';
+import ForgotEmailPassword from './components/auth/ForgotEmailPassword';
+//import HandView from "../../ui/gameroom"
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      loggedIn: false,
-      email: null
-    }
+	constructor() {
+		super();
+		this.state = {
+			loggedIn: false,
+			email: null,
+		};
 
-    this.getUser = this.getUser.bind(this)
-    this.componentDidMount = this.componentDidMount.bind(this)
-    this.updateUser = this.updateUser.bind(this)
-  }
+		this.getUser = this.getUser.bind(this);
+		this.componentDidMount = this.componentDidMount.bind(this);
+		this.updateUser = this.updateUser.bind(this);
+	}
 
-  componentDidMount() {
-    this.getUser()
-  }
+	componentDidMount() {
+		this.getUser();
+	}
 
-  updateUser (userObject) {
-    this.setState(userObject)
-  }
+	updateUser(userObject) {
+		this.setState(userObject);
+	}
 
-  getUser() {
-    try{
-    axios.get('/').then(response => {
-      console.log('Get user response: ')
-      console.log(response.data)
-      if (response.data.user) {
-        console.log('Get User: There is a user saved in the server session: ')
+	getUser() {
+		try {
+			axios.get('/').then((response) => {
+				console.log('Get user response: ');
+				//console.log(response.data)
+				if (response.data.user) {
+					console.log(
+						'Get User: There is a user saved in the server session: '
+					);
 
-        this.setState({
-          loggedIn: true,
-          email: response.data.user.email
-        })
-      } else {
-        console.log('Get user: no user');
-        this.setState({
-          loggedIn: false,
-          email: null
-        })
-      }
-    })
-  }
-  catch (err) {
-    console.warn(err);
-  }
-  }
+					this.setState({
+						loggedIn: true,
+						email: response.data.user.email,
+					});
+				} else {
+					console.log('Get user: no user');
+					this.setState({
+						loggedIn: false,
+						email: null,
+					});
+				}
+			});
+		} catch (err) {
+			console.warn(err);
+		}
+	}
 
-  render() {
-    return (
-        <Router>
-          <div className="App">
-            <Route exact path="/" component={Welcome} />
-            <Route exact path="/register" component={Register} />
-            <Route exact path="/login" render={() => <Login updateUser={this.updateUser}/>} />
-            <Route exact path="/dashboard" component={Dashboard} />
-            <Route exact path="/host" component={Host} />
-            <Route exact path="/join" component={Join} />
-          </div>
-        </Router>
-
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Route exact path="/" component={Welcome} />
+					<Route exact path="/register" component={Register} />
+					<Route
+						exact
+						path="/login"
+						render={() => <Login updateUser={this.updateUser} />}
+					/>
+					<Route exact path="/dashboard" component={Dashboard} />
+					<Route exact path="/host" component={Host} />
+					<Route exact path="/join" component={Join} />
+					<Route exact path="/forgot" component={ForgotEmailPassword} />
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
