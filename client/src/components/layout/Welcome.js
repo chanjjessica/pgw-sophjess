1f52990bfc6a1c8df883ff2c5c7a7ca6c38056ef
Sophia Tu <sophiatu217@gmail.com>
2021-01-22 20:20:32 -0500

    Updated Welcome page to new design

--- client/src/components/layout/Welcome.js
+++ client/src/components/layout/Welcome.js
@@ -1,46 +1,92 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 // import { Button } from 'react-bootstrap';
-import "./Welcome.css"
+import "./Welcome.css";
 
 class Welcome extends Component {
-    render() {
-        return(
-<div>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Web 1920 – 11</title>
-        
-        {/* <link rel="stylesheet" href="web192011.css"/> */}
-    </head> 
-​
-    <body>
-        <div data-layer="cb5e68ae-2a75-4c89-ab44-9b833016b841" className="web192011">        <div data-layer="2b79d6fa-f987-4908-ba55-549cb5699ec5" className="homescreen4"></div>
-        <div data-layer="e78c8d2b-3488-4e23-97cf-853c11aac0a4" className="rectangle11"></div>
-        <div data-layer="37387328-2cd9-4065-8179-91d881acddf2" className="playGameworksLtdCopyright2020">Play Gameworks Ltd. Copyright 2020</div>
-        <div data-layer="e6c0f44e-6442-4d6d-9748-6b4e5796f12b" className="rectangle19"></div>
-        <div data-layer="5cff7b58-b974-413f-8350-af48ca0ccf1e" className="x1600349927843"></div>
-        <div data-layer="35994f84-efb4-456a-b256-417641076524" className="playmahjong"></div>
-        
-        <Link to="/login" data-layer="bc9bf4fc-9259-4779-bc3c-ffd185168871" className="btn-flat waves-effect waves-purple no-uppercase component161">      
-    
-            <div data-layer="edafc455-d8cc-4061-99a4-84b3ffe01335" className="login">Login</div>
-            <div data-layer="0de21636-e118-4a3c-8a8d-834558924d54" className="rectangle20"></div>
+  render() {
+    return (
+      <div>
+        <head>
+          <meta charset="utf-8" />
+          <title>Web 1920 – 12</title>
 
-        </Link>
+          <link rel="stylesheet" href="web192012.css" />
+        </head>
 
-        <Link to="/register" data-layer="1e22b445-b71f-4c6b-8f7a-55299f0a2b4b" className="btn-flat waves-effect waves-purple no-uppercase component171">            
-            <div data-layer="704d23e8-4c16-4936-9299-2c87c5400054" className="signUp">Sign up</div>
-            <div data-layer="f75b5661-4edd-42ef-9cb9-ba8582a07b2e" className="rectangle21"></div>
-        </Link>
-</div>
-​
-​
-    
-    </body>
-</div>
-        );
-    }
+        <body>
+          <div
+            data-layer="1fd0f7b6-561a-4ea6-a427-4ebed187a2b0"
+            className="web192012"
+          >
+            <div
+              data-layer="6f1d7962-efa7-4b1d-af88-0da6c804059e"
+              className="homescreen4"
+            ></div>
+            <div
+              data-layer="17180e76-d88e-44f9-9124-1af63577eca9"
+              className="rectangle11"
+            ></div>
+            <div
+              data-layer="f68a10ed-7de7-445a-9210-ddd2d77f3982"
+              className="group1527"
+            ></div>
+            <div
+              data-layer="9916eb17-0e7a-48f6-90e9-e45afdbc5c37"
+              className="playGameworksLtdCopyright2020"
+            >
+              Play Gameworks Ltd. Copyright 2020
+            </div>
+            <div
+              data-layer="ba953210-1095-4d2e-9a34-d92ea75337df"
+              className="rectangle19"
+            ></div>
+            <div
+              data-layer="9321d425-fa7b-4054-9e56-f7234bae88d2"
+              className="x1600349927843"
+            ></div>
+            <Link
+              to="/login"
+              data-layer="edfca9e8-2336-4a08-9e2f-368ca2e2cc7b"
+              className="component162"
+            >
+              <div
+                to="/login"
+                data-layer="c2ce21b0-b57f-44af-bdea-4a2a6e24215e"
+                className="login"
+              >
+                Login
+              </div>
+              <div
+                data-layer="bf012c1e-3774-48bb-acf9-2bf88a784a49"
+                className="rectangle20"
+              ></div>
+            </Link>
+            <Link
+              to="/register"
+              data-layer="903ffd7c-ad0d-49f4-9e22-87e7a130b9f2"
+              className="component172"
+            >
+              <div
+                data-layer="3cca26e6-3ef2-48ea-aaf6-445c86c4bbbc"
+                className="signUp"
+              >
+                Sign up
+              </div>
+              <div
+                data-layer="6b2fad6e-3f31-4739-a3fc-ebb78a85ca7a"
+                className="rectangle21"
+              ></div>
+            </Link>
+            <div
+              data-layer="65797876-5100-4852-9952-b899bb30f184"
+              className="img3256"
+            ></div>
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default Welcome;
